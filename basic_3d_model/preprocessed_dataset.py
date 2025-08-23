bf7d0868b4fec73733bcc74cf58504c78e3bd82f
lefixhn <fedo2004@gmail.com>
2025-08-23 09:45:19 +0000

    neues update

--- basic_3d_model/preprocessed_dataset.py
+++ basic_3d_model/preprocessed_dataset.py
@@ -2,7 +2,7 @@ from torch.utils.data import Dataset
 import numpy as np 
 import torch 
 
-clas PreprocessedDataset(Dataset): 
+class PreprocessedDataset(Dataset): 
     '''Expects the given paths to direct to .npy files. '''
     def __init__(self, 
     hr_paths=[f'/content/drive/MyDrive/superresolution_3d_data/datasets/superresolution_brats/hr_images/hr_image{i:03d}.npy' for i in range(0, 484)], 
@@ -12,9 +12,9 @@ clas PreprocessedDataset(Dataset):
         print("PRELOADING DATA")
         assert len(lr_paths) == len(hr_paths)
         self.data = [
-            (self._get_image_as_tensor_(hr_paths[i]), 
-            self._get_image_as_tensor_(lr_paths[i]) )
-            ) for i in range(0, len(hr_paths)
+            (self._get_image_as_tensor_(lr_paths[i]), 
+            self._get_image_as_tensor_(hr_paths[i]) )
+            for i in range(0, len(hr_paths)
         ]
         print("DATA LOADED")
 
@@ -29,5 +29,6 @@ clas PreprocessedDataset(Dataset):
         image = image.astype(np.float32, copy=False)
         tensor = torch.from_numpy(image)
         # Add channel dimension 
-        tensor = tensor.unsqueeze(0)
+        if tensor.ndim == 3: 
+            tensor = tensor.unsqueeze(0)
         return tensor
