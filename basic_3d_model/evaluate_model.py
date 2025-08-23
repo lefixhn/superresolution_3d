6e6eaea1799994020b59c11d00bb3ccc0acc2c03
lefixhn <fedo2004@gmail.com>
2025-08-23 11:31:32 +0000

    neues update

--- basic_3d_model/evaluate_model.py
+++ basic_3d_model/evaluate_model.py
@@ -1,17 +1,23 @@
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 from torchmetrics.image.ssim import StructuralSimilarityIndexMeasure
+from model import FirstRes3DModel
+import dataset as ds
 
-
-def reduce_shape_3d(image): 
-    while image.ndim > 3: 
-        image.shqueeze(0)
+def make_shape_5d(image): 
+    while image.ndim < 5: 
+        image = image.unsqueeze(0)
     return image
 
-def evaluate_model(model, validation_dataset: Dataset, name='MyModel'):
+@torch.no_grad()
+def evaluate_model(model, validation_dataset: Dataset, name='MyModel', 
+device= 'cuda' if torch.cuda.is_available() else 'cpu'
+):
+    
+    model = model.to(device).eval()
 
-    dataloader = DataLoader(dataset=dataset)
+    dataloader = DataLoader(dataset=validation_dataset)
     
     sample_count = 0
     mse = 0
@@ -25,16 +31,19 @@ def evaluate_model(model, validation_dataset: Dataset, name='MyModel'):
     
 
     for lr_image, hr_image in dataloader: 
-        # Reduce to 3D Shape 
-        lr_image = reduce_shape_3d(lr_image)
-        hr_image = reduce_shape_3d(hr_image)
+        # Ensure the images have a 5D shape (Batch, Channel, Depth, Height, Width)
+        lr_image = make_shape_5d(lr_image)
+        hr_image = make_shape_5d(hr_image)
+        # Move to device 
+        lr_image = lr_image.to(device)
+        hr_image = hr_image.to(device)
 
         # Calculate SR Result
         sr_image = model(lr_image)
 
-        mse += mse_metric(sr_image, hr_image)
-        mae += mae_metric(sr_image, hr_image)
-        ssim += ssim_metric(sr_image, hr_image)
+        mse += mse_metric(sr_image, hr_image).item()
+        mae += mae_metric(sr_image, hr_image).item()
+        ssim += ssim_metric(sr_image, hr_image).item()
         
         sample_count += 1
     
@@ -48,7 +57,15 @@ def evaluate_model(model, validation_dataset: Dataset, name='MyModel'):
     print(f'MAE: {mae}')
     print(f'SSIM: {ssim}')
 
+if __name__ == '__main__': 
+    dataset = ds.Dataset3DMri(
+        paths=ds.generate_paths(start_index=3, end_index=4)
+    )
 
+    model = FirstRes3DModel()
+    checkpoint = torch.load('/content/drive/MyDrive/superresolution_3d_data/models/MyModel')
+    model.load_state_dict(checkpoint)
+    evaluate_model(model=model, validation_dataset=dataset)
 
         
 
