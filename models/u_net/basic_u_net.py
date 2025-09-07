8d2e025683c010a4348766d0ca86b82701b0360f
lefixhn <fedo2004@gmail.com>
2025-09-07 12:03:14 +0000

    bisschen modell gebastelt. bisschen repariri

--- models/u_net/basic_u_net.py
+++ models/u_net/basic_u_net.py
@@ -5,6 +5,29 @@ import torch.nn.functional as F
 # Data Structure: 
 # 5D Tensor (BatchIndex, Channel, Depth, Height, Width)
 
+
+class PixelShuffle3D(nn.Module):
+    def __init__(self, upscale_factor):
+        super().__init__()
+        self.upscale_factor = upscale_factor
+
+    def forward(self, x):
+        batch_size, channels, depth, height, width = x.size()
+        r = self.upscale_factor
+
+        # Anzahl der Output-Kanäle
+        out_channels = channels // (r ** 3)
+
+        # Umformen: Kanäle in r³ aufteilen
+        x = x.view(batch_size, out_channels, r, r, r, depth, height, width)
+
+        # Umsortieren: (B, C, r, r, r, D, H, W) → (B, C, D*r, H*r, W*r)
+        x = x.permute(0, 1, 5, 2, 6, 3, 7, 4).contiguous()
+        x = x.view(batch_size, out_channels, depth * r, height * r, width * r)
+
+        return x
+
+
 class DenseBlock3D(nn.Module): 
 
     def __init__(self, in_channels, out_channels=None,num_layers=4, l_lrelu_alpha=0.1): 
@@ -77,22 +100,42 @@ class BasicUNetU(nn.Module):
         ])
 
         decoder_level2_in_channels = level3_out_channels + level2_out_channels
+        decoder_level2_out_channels = level2_out_channels
+
+        decoder_level1_in_channels = decoder_level2_out_channels + level1_out_channels
+        decoder_level1_out_channels = level1_out_channels
+        
+
        
         self.decoder_level2 = nn.Sequential([
-            nn.Conv3d(in_channels=decoder_level2_in_channels, out_channels=level3_out_channels), 
+            nn.Conv3d(in_channels=decoder_level2_in_channels, out_channels=decoder_level2_out_channels), 
             nn.LeakyReLU(0.1),
-            nn.Conv3d(in_channels=level3_out_channels, out_channels=level3_out_channels), 
+            nn.Conv3d(in_channels=decoder_level2_out_channels, out_channels=decoder_level2_out_channels), 
             nn.LeakyReLU(0.1),
-            nn.Conv3d(in_channels=level3_out_channels, out_channels=level3_out_channels), 
+            nn.Conv3d(in_channels=decoder_level2_out_channels, out_channels=decoder_level2_out_channels), 
             nn.LeakyReLU(0.1),
-            nn.Conv3d(in_channels=level3_out_channels, out_channels=level3_out_channels), 
+            nn.Conv3d(in_channels=decoder_level2_out_channels, out_channels=decoder_level2_out_channels), 
             nn.LeakyReLU(0.1),
+        ])
+
+        self.decoder_level1 = nn.Sequential([
+            nn.Conv3d(in_channels=decoder_level1_in_channels, out_channels=decoder_level1_out_channels), 
+            nn.LeakyReLU(0.1),
+            nn.Conv3d(in_channels=decoder_level1_out_channels, out_channels=decoder_level1_out_channels), 
+            nn.LeakyReLU(0.1),
+            nn.Conv3d(in_channels=decoder_level1_out_channels, out_channels=decoder_level1_out_channels), 
+            nn.LeakyReLU(0.1),
+            nn.Conv3d(in_channels=decoder_level1_out_channels, out_channels=decoder_level1_out_channels), 
+            nn.LeakyReLU(0.1)
         ])
         
     
     def forward(self, x):
         
+        encoder_level1_out = self.encoder_level1(x)
         
+        encoder_level2_out = self.encoder_level2(encoder_level1_out)
+        encoder_level3_out = self.encoder_level2(encoder_level2_out)
         
 
 
