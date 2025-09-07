fb677f8b444273892ab076636287a56fce4a48cb
lefixhn <fedo2004@gmail.com>
2025-09-07 09:59:01 +0000

    bisschen modell gebastelt. bisschen repariri

--- models/u_net/basic_u_net.py
+++ models/u_net/basic_u_net.py
@@ -6,20 +6,23 @@ import torch
 
 class DenseBlock3D(nn.Module): 
 
-    def __init__(self, in_channels ,layers=4 ): 
+    def __init__(self, in_channels ,num_layers=4, l_lrelu_alpha=0.1): 
         super().__init__()
         # The layers have growing input sizes due to concatination, but the
         # Same output size 
-        self.layers = layers
-        self.layers = [
-            nn.Conv3d(in_channels*i, in_channels)
-            for i in range(1, layers+1)
-        ]
-        self.l_relu = nn.LeakyReLU(0.1) 
+        self.num_layers = num_layers
+        self.layers = nn.ModuleList([
+            nn.Sequential([
+                nn.Conv3d(in_channels*i, in_channels)
+                nn.LeakyReLU(l_lrelu_alpha)
+            ])
+            for i in range(1, num_layers+1)
+        ])
+        
     
     def forward(self, x): 
         for layer in self.layers: 
-            x = torch.cat(x, layer(x))
+            x = torch.cat([x, layer(x)], dim=1)
         return x
 
 
