ab1594ca4a36860759ca2c4e747bf76a097d5b2b
lefixhn <fedo2004@gmail.com>
2025-09-07 12:16:34 +0000

    bisschen modell gebastelt. bisschen repariri

--- models/u_net/basic_u_net.py
+++ models/u_net/basic_u_net.py
@@ -5,29 +5,6 @@ import torch.nn.functional as F
 # Data Structure: 
 # 5D Tensor (BatchIndex, Channel, Depth, Height, Width)
 
-
-class PixelShuffle3D(nn.Module):
-    def __init__(self, upscale_factor):
-        super().__init__()
-        self.upscale_factor = upscale_factor
-
-    def forward(self, x):
-        batch_size, channels, depth, height, width = x.size()
-        r = self.upscale_factor
-
-        # Anzahl der Output-Kanäle
-        out_channels = channels // (r ** 3)
-
-        # Umformen: Kanäle in r³ aufteilen
-        x = x.view(batch_size, out_channels, r, r, r, depth, height, width)
-
-        # Umsortieren: (B, C, r, r, r, D, H, W) → (B, C, D*r, H*r, W*r)
-        x = x.permute(0, 1, 5, 2, 6, 3, 7, 4).contiguous()
-        x = x.view(batch_size, out_channels, depth * r, height * r, width * r)
-
-        return x
-
-
 class DenseBlock3D(nn.Module): 
 
     def __init__(self, in_channels, out_channels=None,num_layers=4, l_lrelu_alpha=0.1): 
@@ -52,6 +29,30 @@ class DenseBlock3D(nn.Module):
         for layer in self.layers: 
             x = torch.cat([x, layer(x)], dim=1)
         return x
+
+
+
+class PixelShuffle3D(nn.Module):
+    def __init__(self, upscale_factor):
+        super().__init__()
+        self.upscale_factor = upscale_factor
+
+    def forward(self, x):
+        batch_size, channels, depth, height, width = x.size()
+        r = self.upscale_factor
+
+        # Anzahl der Output-Kanäle
+        out_channels = channels // (r ** 3)
+
+        # Umformen: Kanäle in r³ aufteilen
+        x = x.view(batch_size, out_channels, r, r, r, depth, height, width)
+
+        # Umsortieren: (B, C, r, r, r, D, H, W) → (B, C, D*r, H*r, W*r)
+        x = x.permute(0, 1, 5, 2, 6, 3, 7, 4).contiguous()
+        x = x.view(batch_size, out_channels, depth * r, height * r, width * r)
+
+        return x
+
 
 
 
@@ -126,17 +127,30 @@ class BasicUNetU(nn.Module):
             nn.Conv3d(in_channels=decoder_level1_out_channels, out_channels=decoder_level1_out_channels), 
             nn.LeakyReLU(0.1),
             nn.Conv3d(in_channels=decoder_level1_out_channels, out_channels=decoder_level1_out_channels), 
-            nn.LeakyReLU(0.1)
+            nn.LeakyReLU(0.1), 
+            # Adjust dimensions for pixelshuffle 
+            nn.Conv3d(in_channels=decoder_level1_out_channels, out_channels=2**upscale_factor), 
         ])
+        
+        self.pixel_shuffle = PixelShuffle3D(upscale_factor=self.upscale_factor)
+
+        
         
     
     def forward(self, x):
-        
+        pool = nn.MaxPool3d(2, 2)
+        upscale = F.interpolate(scale_factor=(self.upscale_factor, self.upscale_factor, self.upscale_factor))
+
         encoder_level1_out = self.encoder_level1(x)
+        encoder_level2_out = self.encoder_level2(pool(encoder_level1_out))
+        encoder_level3_out = self.encoder_level2(pool(encoder_level2_out))
         
-        encoder_level2_out = self.encoder_level2(encoder_level1_out)
-        encoder_level3_out = self.encoder_level2(encoder_level2_out)
-        
+        decoder_level2_in = torch.cat([upscale(encoder_level3_out), encoder_level2_out], dim=1)
+        decoder_level_2_out = self.decoder_level2(decoder_level2_in)
+        devoder_level_1_in = torch.cat([upscale(decoder_level_2_out), encoder_level1_out])
+        devoder_level_1_out = self.decoder_level1(devoder_level_1_in)
+        out = self.pixel_shuffle(devoder_level_1_out)
+        return out
 
 
         
