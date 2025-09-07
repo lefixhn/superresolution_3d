254696819b2d15aeb413370c88ea008c156961b8
lefixhn <fedo2004@gmail.com>
2025-09-07 09:51:39 +0000

    bisschen modell gebastelt

--- models/u_net/basic_u_net.py
+++ models/u_net/basic_u_net.py
@@ -10,14 +10,18 @@ class DenseBlock3D(nn.Module):
         super().__init__()
         # The layers have growing input sizes due to concatination, but the
         # Same output size 
+        self.layers = layers
         self.layers = [
             nn.Conv3d(in_channels*i, in_channels)
             for i in range(1, layers+1)
         ]
-        
-
+        self.l_relu = nn.LeakyReLU(0.1) 
     
     def forward(self, x): 
+        for layer in self.layers: 
+            x = torch.cat(x, layer(x))
+        return x
+
 
 
 class BasicUNetU(nn.Module): 
