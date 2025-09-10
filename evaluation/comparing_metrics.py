a5d0a484de1b2aab731e4ceb512c2c729c4ddb6b
lefixhn <fedo2004@gmail.com>
2025-09-10 15:28:49 +0000

    preprocessing skript angepasst, damit man durch größere zahlen dividieren kann

---
+++ evaluation/comparing_metrics.py
@@ -0,0 +1,15 @@
+
+def compare_mse(sr_image, hr_image): 
+    return None
+
+def compare_mae(sr_image, hr_image): 
+    return None
+
+def compare_psnr(sr_image, hr_image): 
+    return None 
+
+def compare_ssim(sr_image, hr_image): 
+    return None
+
+def compare_lpips(sr_image, hr_image): 
+    return None 
