8ed3ce913781abfb446e9c3db6161c4cd8bc98f8
lefixhn <fedo2004@gmail.com>
2025-09-11 21:16:25 +0000

    preprocessing skript angepasst, damit man durch größere zahlen dividieren kann

--- evaluation/comparing_metrics.py
+++ evaluation/comparing_metrics.py
@@ -1,15 +1,22 @@
+import torch 
+import torch.nn.functional as F  
+
 
 def compare_mse(sr_image, hr_image): 
-    return None
+    return F.mse_loss(sr_image, hr_image).item()
+
 
 def compare_mae(sr_image, hr_image): 
     return None
 
+
 def compare_psnr(sr_image, hr_image): 
     return None 
 
+
 def compare_ssim(sr_image, hr_image): 
     return None
+
 
 def compare_lpips(sr_image, hr_image): 
     return None 
