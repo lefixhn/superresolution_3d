70891f4e782f3d887c5845246fad3acdd20d9983
lefixhn <fedo2004@gmail.com>
2025-09-15 12:18:54 +0000

    comparing_metrics wird geschrieben bugs gefixt

--- models/dense_net/basic_efficient_dense_net.py
+++ models/dense_net/basic_efficient_dense_net.py
@@ -43,62 +43,14 @@ class DenseUnit(nn.Module):
             x = self.extraction_convolution(self.extraction_activation(x))
         else: 
             x = self.extraction_activation(self.extraction_convolution(x))
-            
+        return x
 
 
 # Attention: the output here are only the newly extraxted features
 class DenseBlock(nn.Module): 
     def __init__(self, in_channels , num_layers=8, growth_rate=12, bottleneck_channels=48, with_batch_norm=True, build_activation_function=None): 
         super().__init__()
-        self.in_channels = in_channels
-        self.num_layers = num_layers
-        self.growth_rate = growth_rate
-        self.bottleneck_channels = bottleneck_channels
         
-        self.bottleneck_layers = nn.ModuleList([
-            nn.Sequential([
-                if with_batch_norm: nn.BatchNorm3D(in_channels + i*growth_rate), 
-
-                nn.Conv3d(
-                in_channels = in_channels + i*growth_rate, 
-                out_channels = bottleneck_channels, 
-                kernel_size = 1, 
-                padding = 0
-            ), 
-
-            ])
-            for i in range(self.num_layers)
-        ])
-
-        self.extraction_layers = nn.ModuleList([
-            nn.Sequential([
-                nn.Conv3d(
-                in_channels = bottleneck_channels, 
-                out_channels = growth_rate, 
-                kernel_size = 3, 
-                padding = 1
-            ), 
-            # Batch Norm is optional
-            if with_batch_norm: nn.BatchNorm3D(bottleneck_channels)
-            # LReLU is default as activation function 
-            if build_activation_function is None nn.LeakyReLU(0.1) else build_activation_function(), 
-            ])
-            for i in range(self.num_layers)
-        ])
-
-    def forward(self, x): 
-        in_features = x
-        out = None 
-        for i in range(self.num_layers): 
-            # Reduce features
-            out = self.bottleneck_layers[i](in_features)
-            # Extract
-            out = self.extraction_layers[i](out)
-            if i < self.num_layers - 1: 
-                # Concatenate for the next layer
-                in_features = torch.cat([in_features, out], dim=1)
-
-        return out
 
 
 class BasicEfficientDenseNet(nn.Module): 
