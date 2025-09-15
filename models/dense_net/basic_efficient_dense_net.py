a3671af4993011c087ec1966680182c4a1545239
lefixhn <fedo2004@gmail.com>
2025-09-15 12:24:28 +0000

    comparing_metrics wird geschrieben bugs gefixt

--- models/dense_net/basic_efficient_dense_net.py
+++ models/dense_net/basic_efficient_dense_net.py
@@ -10,7 +10,7 @@ class DenseUnit(nn.Module):
         self.build_activation_function = build_activation_function
         self.with_batch_norm = with_batch_norm
 
-        self.bottleneck_batch_norm = nn.BatchNorm3D(in_channels)
+        self.bottleneck_batch_norm = nn.BatchNorm3d(in_channels)
         self.bottleneck_activation = nn.LeakyReLU(0.1) if build_activation_function is None else build_activation_function()
         self.bottleneck_convolution = nn.Conv3d(
             in_channels = in_channels,
@@ -19,7 +19,7 @@ class DenseUnit(nn.Module):
             padding = 0
         )
 
-        self.extraction_batch_norm = nn.BatchNorm3D(in_channels)
+        self.extraction_batch_norm = nn.BatchNorm3d(bottleneck_channels)
         self.extraction_activation = nn.LeakyReLU(0.1) if build_activation_function is None else build_activation_function()
         self.extraction_convolution = nn.Conv3d(
             in_channels = bottleneck_channels, 
@@ -29,17 +29,17 @@ class DenseUnit(nn.Module):
         )
     def forward(self, x): 
         # BOTTLENECK LAYER 
-        if with_batch_norm: 
+        if self.with_batch_norm: 
             x = self.bottleneck_batch_norm(x)
         # Control the order in wich the operations are being executed 
-        if pre_activation: 
+        if self.pre_activation: 
             x = self.bottleneck_convolution(self.bottleneck_activation(x))
         else: 
             x = self.bottleneck_activation(self.bottleneck_convolution(x))
         # EXTRACTION LAYER
-        if with_batch_norm: 
+        if self.with_batch_norm: 
             x = self.extraction_batch_norm(x)
-        if pre_activation: 
+        if self.pre_activation: 
             x = self.extraction_convolution(self.extraction_activation(x))
         else: 
             x = self.extraction_activation(self.extraction_convolution(x))
@@ -48,9 +48,26 @@ class DenseUnit(nn.Module):
 
 # Attention: the output here are only the newly extraxted features
 class DenseBlock(nn.Module): 
-    def __init__(self, in_channels , num_layers=8, growth_rate=12, bottleneck_channels=48, with_batch_norm=True, build_activation_function=None): 
+    def __init__(self, in_channels, num_layers=8, growth_rate=12, bottleneck_channels=48, with_batch_norm=True, build_activation_function=None): 
         super().__init__()
-        
+        self.in_channels = in_channels
+        self.num_layers = num_layers
+        self.growth_rate = growth_rate
+        self.bottleneck_channels = bottleneck_channels
+        self.with_batch_norm = with_batch_norm
+        self.build_activation_function = build_activation_function
+
+        self.dense_units = nn.ModuleList(
+            [
+                DenseUnit(
+                    in_channels = in_channels + i*growth_rate, 
+                    bottleneck_channels = bottleneck_channels, 
+                    growth_rate = growth_rate, 
+                    
+                )
+            ]
+            for i in range(self.num_layers)
+        )
 
 
 class BasicEfficientDenseNet(nn.Module): 
