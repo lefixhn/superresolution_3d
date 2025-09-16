c92f59fd79ce879fccb867e3f06faa2a5442e56a
lefixhn <fedo2004@gmail.com>
2025-09-16 15:18:47 +0000

    Dense modell speicherverbesserung

--- models/dense_net/basic_efficient_dense_net.py
+++ models/dense_net/basic_efficient_dense_net.py
@@ -220,13 +220,13 @@ if __name__ == "__main__":
     print("Checking weather model works")
     # Check weather this works
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model = BasicEfficientDenseNet(num_dense_blocks=8, num_units_per_dense_block=8)
+    model = BasicEfficientDenseNet(num_dense_blocks=4, num_units_per_dense_block=4)
     model.to(device)
     
-    x = torch.randn(2, 1, 64, 64, 64)  # [B,C,D,H,W], D/H/W % 4 == 0
+    x = torch.randn(2, 1, 256, 256, 128)  # [B,C,D,H,W], D/H/W % 4 == 0
     x = x.to(device)
     y = model(x)
-    print("in :", x.shape)  # torch.Size([2, 1, 64, 64, 64])
+    print("in :", x.shape)  # torch.Size([2, 1, 512, 512, 256])
     print("out:", y.shape)  # Erwartet: [2, 1, 128, 128, 128]
     assert y.shape == (2, 1, 128, 128, 128)
 
