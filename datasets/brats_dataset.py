99d33201b29319551475154351bf1a76da5de35c
lefixhn <fedo2004@gmail.com>
2025-09-17 09:34:14 +0000

    Training verbessert

--- datasets/brats_dataset.py
+++ datasets/brats_dataset.py
@@ -56,7 +56,7 @@ class PreloadedBratsDataset(Dataset):
         return tensor 
 
 
-class LazyLoadingDataset(Dataset): 
+class LazyLoadingBratsDataset(Dataset): 
     def __init__(
         self,
         cube_side_length=None,
@@ -85,7 +85,6 @@ class LazyLoadingDataset(Dataset):
         else: 
             raise Exception(f"The given .npy file has {image.ndim} dimensions")
 
-
     # 4d > (Channel, x, y, z)
     def __getitem__(self, index):
         # Use memory mapping to only load a smaller part of the file 
@@ -96,22 +95,24 @@ class LazyLoadingDataset(Dataset):
             lr_tensor = self._convert_to_4d_tensor(np.asarray(lr_memory_map, dtype=np.float32))
             hr_tensor = self._convert_to_4d_tensor(np.asarray(hr_memory_map, dtype=np.float32))
             return (lr_tensor, hr_tensor)
-        
-        
-        lr_shape = data_tuple[0].shape[1:]
-        min_shape_dim = min(lr_shape)
-        safe_side_length = int(min([min_shape_dim, self.cube_side_length]))
-        # Select random position 
-        x, y, z = np.random.randint(0, lr_shape[0] - safe_side_length + 1), np.random.randint(0, lr_shape[1] - safe_side_length + 1), np.random.randint(0, lr_shape[2] - safe_side_length + 1)
-        # Select Sub cubes
-        lr_tensor = data_tuple[0][:, x:x+safe_side_length, y:y+safe_side_length, z:z+safe_side_length]
-        x, y, z, safe_side_length = 2*x, 2*y, 2*z, 2*safe_side_length
-        hr_tensor = data_tuple[1][:, x:x+safe_side_length, y:y+safe_side_length, z:z+safe_side_length]
+        else:
+            lr_shape = lr_memory_map.shape
+            safe_side_length = int(min(list(lr_shape) + [self.cube_side_length]))
+            x = np.random.randint(0, lr_shape[0] - safe_side_length + 1)
+            y = np.random.randint(0, lr_shape[1] - safe_side_length + 1)
+            z = np.random.randint(0, lr_shape[2] - safe_side_length + 1)
+            # Fetch required information from the lr memory map 
+            lr_image = lr_memory_map[x:x+safe_side_length, y:y+safe_side_length, z:z+safe_side_length]
+            # Calculate the hr_coordinates
+            x, y, z, safe_side_length = 2*x, 2*y, 2*z, 2*safe_side_length
+            # Fetch required information from the hr memory map 
+            hr_iamge = hr_memory_map[x:x+safe_side_length, y:y+safe_side_length, z:z+safe_side_length]
+            # Convert to tensor
+            lr_tensor = self._convert_to_4d_tensor(lr_image)
+            hr_tensor = self._convert_to_4d_tensor(hr_image)
+            return (lr_tensor, hr_tensor)
 
 
 
-        
-        
-        
-
+      
 
