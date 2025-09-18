42532491b52d480461fe1d1f42895ef64cbc7b40
lefixhn <fedo2004@gmail.com>
2025-09-18 10:53:33 +0000

    Vergleichsskript fehler bei lipis wegen device gefixt

--- evaluation/comparing_metrics.py
+++ evaluation/comparing_metrics.py
@@ -90,18 +90,15 @@ def compare_ssim(sr, hr, data_range=None, ks=11, sigma=1.5, K1=0.01, K2=0.03, ep
         den = (mu_x2 + mu_y2 + C1) * (sigma_x2 + sigma_y2 + C2) + eps
         ssim_map = num / den
         return float(ssim_map.mean().item())
-        
+
 @torch.inference_mode()
 def compare_lpips(sr_image, hr_image, lpips_2d_metric=None):
     target_device = 'cuda' if torch.cuda.is_available() else 'cpu'
     # Build the lpips metric
     if lpips_2d_metric is None: 
         lpips_2d_metric=LPIPS(network='vgg').eval().to(target_device)
-
     # Convert to 5D Tensors
     sr_image, hr_image = _convert_to_5d_tensor(sr_image), _convert_to_5d_tensor(hr_image) 
-    # Store deivces of both 
-    former_sr_device, former_hr_device = sr_image.device, hr_image.device
     # Bring both images to target device
     sr_image, hr_image = sr_image.to(target_device), hr_image.to(target_device)
     # Not nessecary for the piqa lipips implementation 
@@ -131,9 +128,7 @@ def compare_lpips(sr_image, hr_image, lpips_2d_metric=None):
             lpips_mean += lpips_sum_over_slices / dimension_length
     # Divide by the amount of orientations and the number of batches
     lpips_mean /= 3 * tensor_shape[0]
-    # Bring tensors back 
-    sr_image = sr_image.to(former_sr_device)
-    hr_image = hr_image.to(former_hr_device)
+    
 
     return lpips_mean
 
