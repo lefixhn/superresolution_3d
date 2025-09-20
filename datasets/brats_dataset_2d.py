6bf75be0b96f876694e2dc5e5a1edd285ed15574
lefixhn <fedo2004@gmail.com>
2025-09-20 09:58:46 +0000

    BasicEfficientDenseNet2d und LazyLoadingBratsDataset2d

--- datasets/brats_dataset_2d.py
+++ datasets/brats_dataset_2d.py
@@ -45,7 +45,7 @@ class LazyLoadingBratsDataset2d(Dataset):
         # Only load the paths 
         self.lr_files = [os.path.join(lr_path, f"{i:04d}.npy") for i in range(start_item_index, start_item_index+item_count)]
         self.hr_files = [os.path.join(hr_path, f"{i:04d}.npy") for i in range(start_item_index, start_item_index+item_count)]
-        print("FINISHED INITIALIZING LazyLoadingBratsDataset")
+        print("FINISHED INITIALIZING LazyLoadingBratsDataset2d")
 
     def __len__(self):
         return len(self.hr_files)
