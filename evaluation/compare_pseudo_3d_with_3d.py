40b692aa0e99b17b49af8fdf5f58c818d3d8fafc
lefixhn <fedo2004@gmail.com>
2025-09-24 14:47:05 +0000

    Dummes slice wise skript

--- evaluation/compare_pseudo_3d_with_3d.py
+++ evaluation/compare_pseudo_3d_with_3d.py
@@ -9,13 +9,46 @@ import torch
 from tqdm import tqdm
 import comparing_metrics as cm 
 from typing import Dict
+from build_sclie_wise_hr_volume import build_sclie_wise_hr_volume
 
-
+@torch.no_grad()
 def compare_pseudo_3d_with_3d(
     model_2d: torch.nn.Module, 
     model_3d: torch.nn.Module, 
     lr_hr_5d_tensor_tuples,
+    device="cuda" if torch.cuda.is_available() else "cpu", 
 ) -> Dict[str, Dict[str, float]]:
+    
     results = {}
+    
+    mean_mse_2d = 0.0
+    mean_mae_2d = 0.0
+    mean_psnr_2d = 0.0
+
+    mean_mse_3d = 0.0
+    mean_mae_3d = 0.0
+    mean_psnr_3d = 0.0
+    
+
+    model_3d = model_3d.to(device)
+
     for lr_volume_tensor_5d, hr_volume_tensor_5d in tqdm(lr_hr_5d_tensor_tuples, "Iterating trhough lr-hr-tuples"):
-         
+        # Build SR Images
+        sr_image_2d_model = build_sclie_wise_hr_volume(
+            lr_volume=lr_volume_tensor_5d, 
+            model: model_2d, 
+            interpolation_dim="D",
+            device=device, 
+        )
+
+        sr_image_3d_model = model_3d(lr_volume_tensor_5d)
+
+        # Apply metrics 
+        mean_mse_2d += cm.compare_mse(sr_image_2d_model ,hr_volume_tensor_5d)
+        mean_mse_2d += cm.compare_mae(sr_image_2d_model ,hr_volume_tensor_5d)
+        mean_mse_2d += cm.compare_psnr(sr_image_2d_model ,hr_volume_tensor_5d)
+
+        mean_mse_3d += cm.compare_mse(sr_image_3d_model ,hr_volume_tensor_5d)
+        mean_mse_3d += cm.compare_mae(sr_image_3d_model ,hr_volume_tensor_5d)
+        mean_mse_3d += cm.compare_psnr(sr_image_3d_model ,hr_volume_tensor_5d)
+
