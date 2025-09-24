48ecbadbeaafc2ab7ac26b0b9c06329e9cc4628d
lefixhn <fedo2004@gmail.com>
2025-09-24 11:01:00 +0000

    Dummes slice wise skript

---
+++ evaluation/build_slice_wise_hr_volume.py
@@ -0,0 +1,17 @@
+'''
+    Uses a 2D SR Model to generate a 3D HR Volume
+    Two axes are upscaled though the 2D Model. 
+    The remaining axe is upscaled though interpolation
+'''
+import torch 
+
+def build_sclie_wise_hr_volume(
+    lr_volume: torch.Tensor, 
+    model: callable, 
+    interpolation_order=1, 
+    upscale_factor=2, 
+    device = "cuda" if torch.cuda.is_available() else "cpu", 
+): 
+    if lr_volume.ndims() == 4: 
+        lr_volume = lr_volume
+    
