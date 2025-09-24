bc0f9d166e114f2ccdbf227ab635f27446189c11
lefixhn <fedo2004@gmail.com>
2025-09-24 14:38:51 +0000

    Dummes slice wise skript

--- evaluation/compare_pseudo_3d_with_3d.py
+++ evaluation/compare_pseudo_3d_with_3d.py
@@ -7,15 +7,15 @@
 ''' 
 import torch
 from tqdm import tqdm
-
-
+import comparing_metrics as cm 
+from typing import Dict
 
 
 def compare_pseudo_3d_with_3d(
     model_2d: torch.nn.Module, 
     model_3d: torch.nn.Module, 
     lr_hr_5d_tensor_tuples,
-):
+) -> Dict[str, Dict[str, float]]:
     results = {}
     for lr_volume_tensor_5d, hr_volume_tensor_5d in tqdm(lr_hr_5d_tensor_tuples, "Iterating trhough lr-hr-tuples"):
          
