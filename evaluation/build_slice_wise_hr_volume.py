0012e4e4422e06a9604081de54f69755c4b9dd8f
lefixhn <fedo2004@gmail.com>
2025-09-24 11:07:51 +0000

    Dummes slice wise skript

--- evaluation/build_slice_wise_hr_volume.py
+++ evaluation/build_slice_wise_hr_volume.py
@@ -9,6 +9,7 @@ def build_sclie_wise_hr_volume(
     lr_volume: torch.Tensor, 
     model: callable, 
     interpolation_order=1, 
+    interpolation_axe_dim=0     # Can be 0, 1 or 2
     upscale_factor=2, 
     device = "cuda" if torch.cuda.is_available() else "cpu", 
 ): 
@@ -17,6 +18,13 @@ def build_sclie_wise_hr_volume(
     elif lr_volume.ndims() == 3: 
         lr_volume = lr_volume.unsqueeze(0).unsqueeze(0)
     elif not lr_volume.ndims() == 5: 
-        raise TypeError("Input must have shape (B, C, D, H, W), ")
+        raise TypeError("Input must have shape (B, C, D, H, W), (C, D, H, W) or (D, H, W)")
+
+    assert lr_volume.shape[1] == 1, "Image must be greyscale"
+
+    B, C, D, H, W = lr_volume.shape
+
+    for batch_index in range(B): 
+        lr_vomume_4d = 
 
     
