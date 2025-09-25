d9d60d3e5e8cb243c97305e069f46c8843817599
lefixhn <fedo2004@gmail.com>
2025-09-25 09:47:50 +0000

    Dummes slice wise skript erweitert

--- evaluation/compare_pseudo_3d_with_3d.py
+++ evaluation/compare_pseudo_3d_with_3d.py
@@ -100,11 +100,13 @@ def compare_pseudo_3d_dense_with_3d(
     degradation_models = build_degradation_models()
     # Load np volumes and convert to 5d tensor 
     hr_volumes_np = load_np_volumes()
-    hr_volumes_tensor_5d = [torch.from_numpy(hr_volume_np).unsqueeze(0).unsqueeze(0) for hr_volume_np in hr_volumes_np]
+    hr_volume_tensors_5d = [torch.from_numpy(hr_volume_np).unsqueeze(0).unsqueeze(0) for hr_volume_np in hr_volumes_np]
 
-    for degradation_model_name, degradation_model in degradation_models.items(): 
-        lr_volumes_tensor_5d = 
-    
+    for index, degradation_model_name, degradation_model in degradation_models.items(): 
+        lr_volume_tensors_5d = [degradation_model(hr_volume_tensor_5d) for hr_volume_tensor_5d in hr_volume_tensors_5d]
+        lr_hr_volume_tensor_tuples = (lr_volume_tensors_5d[i], hr_volume_tensors_5d[i] for i in range(len(lr_volume_tensors_5d)))
+
+        
 
 
 
