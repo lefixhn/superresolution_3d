c421efe57c948fc94af87fedbea25f677f15e58e
lefixhn <fedo2004@gmail.com>
2025-09-27 13:18:18 +0000

    LPIPS metrik wird angepasst, sodass D, H, W als summatioonsachse angegeben werden kann

--- evaluation/comparing_metrics.py
+++ evaluation/comparing_metrics.py
@@ -142,14 +142,14 @@ def compare_lpips(sr_image, hr_image, lpips_2d_metric=None, compare_axis="D", sl
         for slice_index in range(0, dimension_length, slice_batch_size): 
             #slice_coordinates = (batch_index, 0) + tuple(slice_index if i == shape_dim_index-2 else slice(None) for i in range(3))
             slice_coordinates = [slice(None)] * 5
-            slice_coordinates[0] = slice(slice_index, min(dimension_length, dimension_length+slice_batch_size))
+            slice_coordinates[0] = batch_index
             slice_coordinates[1] = 0 # Grey channel
-            slice_coordinates[shape_dim_index] = slice_index
+            slice_coordinates[shape_dim_index] = slice(slice_index, min(dimension_length, dimension_length+slice_batch_size))
             sr_slice = sr_image[slice_coordinates]
             hr_slice = hr_image[slice_coordinates]
             # Add add channel dimension and triple it
-            sr_slice = sr_slice.unsqueeze(1).expand(-1, 3, -1, -1)
-            hr_slice = hr_slice.unsqueeze(1).expand(-1, 3, -1, -1)
+            sr_slice = sr_slice.unsqueeze(1).repeat(1, 3, 1, 1)
+            hr_slice = hr_slice.unsqueeze(1).expand(1, 3, 1, 1)
             lpips_mean += lpips_2d_metric(sr_slice, hr_slice)
 
     lpips_mean /= batch_size*dimension_length
