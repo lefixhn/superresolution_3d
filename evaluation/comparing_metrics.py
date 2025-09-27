b49af76de0818ecdebd75f2ae12d7f091358b23a
lefixhn <fedo2004@gmail.com>
2025-09-27 12:09:32 +0000

    Hinzufügen von compare_mutual_information in compare_metrics.py

--- evaluation/comparing_metrics.py
+++ evaluation/comparing_metrics.py
@@ -42,12 +42,13 @@ def compare_psnr(sr_image, hr_image):
     return 20 * np.log10(max_element) - 10 * np.log10(compare_mse(sr_image, hr_image ))
 
 
-def compare_mutual_information(sr_iamge, hr_image, bins=100): 
+def compare_mutual_information(sr_image, hr_image, bins=100): 
     from skimage.metrics import normalized_mutual_information as nmi
     sr_image, hr_image = _convert_to_5d_tensor(sr_image), _convert_to_5d_tensor(hr_image)
+    nmi_value=0.0
     for batch_index in range(sr_iamge.shape[0]):
         # Convert to numpy
-        sr_image_np, hr_image_np =  sr_image[batch_index, :, :, :, :].cpu().numpy() , hr_image[batch_index, :, :, :, :].cpu().numpy()
+        sr_image_np, hr_image_np =  sr_image[batch_index, :, :, :, :].detach().cpu().numpy() , hr_image[batch_index, :, :, :, :].detach().cpu().numpy()
         nmi_value += nmi(sr_image_np, hr_image_np, bins = bins)
     nmi_value /= sr_iamge.shape[0]
     return nmi_value
